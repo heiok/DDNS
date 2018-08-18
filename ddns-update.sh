cfd72e1be873241e0ab83909cee252b7c136612e
meizu <meizu@meizudeMacBook-Air.local>
2018-08-19 00:17:00 +0800

    ddns-update.sh

---
+++ ddns-update.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#device name
+device=pppoe-wan
+#ip addr
+IP=''
+#log file
+log=/tmp/ddns.log
+
+#check WAN IP Addr
+checkip()
+{
+    if [ ! -f $log ];then
+        touch $log
+    fi
+    IP=`ifconfig $device | grep 'inet addr' | awk -F ':' '{print $2}' | awk '{print $1}'`
+    OLD=`awk -F '-' 'END {print $2}' $log`
+    if [ "$IP" = "$OLD" ];then
+        IP=''
+    fi
+}
+
+checkip
+if [ "$IP" != "" ];then
+    #update log file
+    echo `date '+%Y/%m/%d %H:%M:%S'`"-$IP" >> $log
+    #update dnspod
+
+fi
+
