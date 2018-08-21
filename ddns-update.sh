8d6f8f91cea2c03a931487f5fe3bd9df34a65bbf
Edison Chan <heiok@love.com>
2018-08-21 15:47:13 +0800

    ddns script update

--- ddns-update.sh
+++ ddns-update.sh
@@ -1,30 +1,17 @@
 #!/bin/sh
-
 #device name
-device=pppoe-wan
-#ip addr
-IP=''
+device=eth0
 #log file
-log=/tmp/ddns.log
-
-#check WAN IP Addr
-checkip()
-{
+log=/home/heiok/ddns.log
     if [ ! -f $log ];then
-        touch $log
+	touch $log
     fi
-    IP=`ifconfig $device | grep 'inet addr' | awk -F ':' '{print $2}' | awk '{print $1}'`
-    OLD=`awk -F '-' 'END {print $2}' $log`
-    if [ "$IP" = "$OLD" ];then
-        IP=''
+#check WAN IP Addr
+    NewIP=`ifconfig $device | grep 'inet addr' | awk -F ':' '{print $2}' | awk '{print $1}'`
+    OldIP=`awk -F '-' 'END {print $2}' $log`
+    if [ "$NewIP" != "$OldIP" ];then
+	#update log file
+    	echo `date '+%Y/%m/%d %H:%M:%S'`"-$NewIP" >> $log
+    	#changeip or dnspod ddns update scripts
+
     fi
-}
-
-checkip
-if [ "$IP" != "" ];then
-    #update log file
-    echo `date '+%Y/%m/%d %H:%M:%S'`"-$IP" >> $log
-    #changeip or dnspod ddns update scripts
-    
-fi
-
