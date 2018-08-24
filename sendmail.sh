898ab4f65b9948201d80294a1a42a697cd8fc2e4
Edison Chan <heiok@love.com>
2018-08-24 10:11:52 +0800

    update mail account and Subject

--- sendmail.sh
+++ sendmail.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-{echo "From:<saminside1@163.com>";
-echo "To:<dgcatv@yeah.net>";
+(echo "From:<sender@mail.com>";
+echo "To:<recipient1@mail.com>;<recipient2@mail.com>";
 echo "Date: `date '+%a, %d %b %Y %H:%M:%S %z'`";
-echo "Subject: OpenWRT Router New IP!";
+echo "Subject: OpenWRT Router New IP is $(ip -4 addr show dev pppoe-wan | awk '/inet / {print $2}' | cut -d/ -f1).";
 echo "";
 #Get current pppoe-wan interface IP
-echo "The new IP is $(ip -4 addr show dev pppoe-wan | awk '/inet / {print $2}' | cut -d/ -f1).";
-}|ssmtp dgcatv@yeah.net
+echo `date '+%Y/%m/%d %H:%M:%S'`"-$(ip -4 addr show dev pppoe-wan | awk '/inet / {print $2}' | cut -d/ -f1).";
+)|ssmtp recipient1@mail.com recipient2@mail.com
